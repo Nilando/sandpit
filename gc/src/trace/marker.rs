b6b6523d2f033914867bbde2be032f3c946e03aa
Lando <naschuma@ucsc.edu>
2024-05-07 20:45:36 -0700

    added set_mark to marker

--- gc/src/trace/marker.rs
+++ gc/src/trace/marker.rs
@@ -1,12 +1,17 @@
 use super::trace::Trace;
-use crate::allocator::{Allocate, GenerationalArena};
+use crate::allocator::{Allocate, GenerationalArena, Marker as AllocMarker};
 use std::ptr::NonNull;
 
 pub trait Marker: Clone {
+    type Mark: AllocMarker;
+
     fn needs_trace<T: Trace>(&self, ptr: NonNull<T>) -> bool;
+    fn set_mark(&mut self, mark: Self::Mark);
 }
 
 impl<A: Allocate> Marker for TraceMarker<A> {
+    type Mark = <<A as Allocate>::Arena as GenerationalArena>::Mark;
+
     fn needs_trace<T: Trace>(&self, ptr: NonNull<T>) -> bool {
         if A::get_mark(ptr) == self.mark {
             return false;
@@ -15,6 +20,10 @@ impl<A: Allocate> Marker for TraceMarker<A> {
         A::set_mark(ptr, self.mark);
 
         T::needs_trace()
+    }
+
+    fn set_mark(&mut self, mark: Self::Mark) {
+        self.mark = mark;
     }
 }
 
