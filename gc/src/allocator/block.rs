365150a560001d940d71bb2786bf670d7e769350
Lando <naschuma@ucsc.edu>
2024-05-17 14:48:39 -0700

    important comment

--- gc/src/allocator/block.rs
+++ gc/src/allocator/block.rs
@@ -5,6 +5,9 @@ use std::ptr::NonNull;
 
 pub type BlockPtr = NonNull<u8>;
 
+// TODO: what if block was just a Box<[u8]> ?
+// I think with that we could remove a ton of unsafe code!
+
 pub struct Block {
     ptr: BlockPtr,
     layout: Layout,
