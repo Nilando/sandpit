de3973e6497ab244750861d92989a5fee1d42322
Nilando <nilandschumacher@gmail.com>
2025-02-19 13:07:45 -0800

    first draft of tagging pointers

---
+++ src/tagged.rs
@@ -0,0 +1,153 @@
+use std::mem::ManuallyDrop;
+use crate::Trace;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use super::gc::{Gc, GcOpt, GcPointer};
+
+pub union Tagged<A: GcPointer> {
+    ptr: ManuallyDrop<A>,
+    raw: ManuallyDrop<AtomicUsize>,
+} 
+
+impl<A: GcPointer> From<A> for Tagged<A> {
+    fn from(value: A) -> Self {
+        Self {
+            ptr: ManuallyDrop::new(value)
+        }
+    }
+}
+
+impl<A: GcPointer> TryFrom<usize> for Tagged<A> {
+    type Error = ();
+
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        if (Self::TAG_MASK & value) == 0 {
+            Err(())
+        } else {
+            Ok(Self {
+                raw: ManuallyDrop::new(AtomicUsize::new(value))
+            })
+        }
+    }
+}
+
+impl<A: GcPointer> Tagged<A> {
+    const TAG_MASK: usize = A::POINTEE_ALIGNMENT - 1;
+
+    pub fn is_ptr(&self) -> bool {
+        self.get_tag() == 0
+    }
+
+    pub fn get_tag(&self) -> usize {
+        unsafe {
+            Self::TAG_MASK & self.raw.load(Ordering::Relaxed)
+        }
+    }
+
+    pub fn get_ptr(&self) -> Option<A> {
+        if self.is_ptr() {
+            unsafe {
+                return Some((*self.ptr).clone())
+            }
+        }
+
+        None
+    }
+
+    pub fn get_raw(&self) -> usize {
+        unsafe {
+            self.raw.load(Ordering::Relaxed)
+        }
+    }
+}
+
+impl<'gc, T: Trace> Tagged<GcOpt<'gc, T>> {
+    pub fn set_raw(&self, value: usize) {
+        assert!(Self::TAG_MASK & value != 0, "Invalid pointer tag");
+
+        unsafe {
+            self.set_raw_unchecked(value);
+        }
+    }
+
+    pub unsafe fn set_raw_unchecked(&self, value: usize) {
+        self.raw.store(value, Ordering::Relaxed);
+    }
+
+    pub unsafe fn set_ptr(&self, ptr: GcOpt<'gc, T>) {
+        self.ptr.set(ptr);
+    }
+}
+
+impl<'gc, T: Trace> Tagged<Gc<'gc, T>> {
+    pub fn set_raw(&self, value: usize) {
+        assert!(Self::TAG_MASK & value != 0, "Invalid pointer tag");
+
+        unsafe {
+            self.set_raw_unchecked(value);
+        }
+    }
+
+    pub unsafe fn set_raw_unchecked(&self, value: usize) {
+        self.raw.store(value, Ordering::Relaxed);
+    }
+
+    pub unsafe fn set_ptr(&self, ptr: Gc<'gc, T>) {
+        self.ptr.set(ptr);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{gc::Gc, Arena, Root};
+
+    #[test]
+    fn test_into_pointer() {
+        let _: Arena<Root![_]> = Arena::new(|mu| {
+            let ptr = Gc::new(mu, 69);
+            let tag = Tagged::from(ptr.clone());
+            let untagged_pointer = tag.get_ptr().unwrap();
+
+            assert!(ptr.as_thin() == untagged_pointer.as_thin());
+        });
+    }
+
+
+    #[test]
+    fn test_into_bytes() {
+        let _: Arena<Root![_]> = Arena::new(|_| {
+            let tagged_bytes = 1;
+            let tag = Tagged::<Gc<usize>>::try_from(tagged_bytes).unwrap();
+            let bytes = tag.get_raw();
+
+            assert!(tagged_bytes == bytes);
+        });
+    }
+
+    #[test]
+    fn test_setting_tag() {
+        let _: Arena<Root![_]> = Arena::new(|mu| {
+            let ptr = GcOpt::new(mu, 69);
+            let tagged_bytes = 7;
+            let tag = Tagged::from(ptr);
+
+            tag.set_raw(tagged_bytes);
+            let bytes = tag.get_raw();
+
+            assert!(tagged_bytes == bytes);
+        });
+    }
+
+    #[test]
+    fn test_invalid_tag() {
+        assert!(Tagged::<Gc<()>>::try_from(0).is_err());
+    }
+
+    #[test]
+    fn test_slice_of_tagged_pointers() {
+        let _: Arena<Root![_]> = Arena::new(|_| {
+            assert!(std::mem::size_of::<Gc<()>>() == std::mem::size_of::<Tagged<Gc<()>>>());
+            assert!(std::mem::size_of::<Gc<()>>() == std::mem::size_of::<Tagged<GcOpt<()>>>());
+        });
+    }
+}
