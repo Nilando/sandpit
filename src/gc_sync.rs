73bf5addcf1a13e23d68664ae6f2c9ef5147e09e
Nilando <nilandschumacher@gmail.com>
2025-06-04 08:23:19 -0700

    allow gcvec to hold cell'ed traceleafs

--- src/gc_sync.rs
+++ src/gc_sync.rs
@@ -1,7 +1,8 @@
 use super::mutator::Mutator;
-use super::trace::Trace;
+use super::trace::{Trace, TraceLeaf};
 use super::gc::{Gc, GcOpt};
 use super::tagged::{Tagged, Tag};
+use std::cell::Cell;
 
 pub trait GcSync<'gc>: Trace + Clone + 'gc {
     fn update_array(mu: &'gc Mutator, array: Gc<'gc, [Self]>, index: usize, value: Self);
@@ -22,5 +23,11 @@ impl<'gc, T: Trace> GcSync<'gc> for GcOpt<'gc, T> {
 impl<'gc, B: Tag + 'gc> GcSync<'gc> for Tagged<'gc, B> {
     fn update_array(mu: &'gc Mutator, array: Gc<'gc, [Self]>, index: usize, value: Self) {
         array.write_barrier(mu, |barrier| barrier.at(index).set(value));
+    }
+}
+
+impl<'gc, T: TraceLeaf + Copy + 'gc> GcSync<'gc> for Cell<T> {
+    fn update_array(mu: &'gc Mutator, array: Gc<'gc, [Self]>, index: usize, value: Self) {
+        array[index].swap(&value);
     }
 }
